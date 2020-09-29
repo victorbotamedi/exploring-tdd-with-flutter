013d9507fb588cbef4491f5969172bd89cc086fa
Victor Botamedi <victor.botamedi@gmail.com>
2020-09-29 18:16:50 -0300

    [Ok] Add note creation UI

--- lib/note_page.dart
+++ lib/note_page.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tdd/cubit/notes_cubit.dart';
 
-class NotePage extends StatelessWidget {
+class NotePage extends StatefulWidget {
   final NotesCubit notesCubit;
 
   const NotePage({Key key, this.notesCubit}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  _NotePageState createState() => _NotePageState();
+}
+
+class _NotePageState extends State<NotePage> {
+  final _titleController = TextEditingController();
+  final _bodyController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                key: ValueKey('title'),
+                controller: _titleController,
+                autofocus: true,
+                decoration: InputDecoration(hintText: 'Title'),
+              ),
+              Expanded(
+                child: TextField(
+                  key: ValueKey('body'),
+                  controller: _bodyController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 500,
+                  decoration:
+                      InputDecoration(hintText: 'Enter your text here...'),
+                ),
+              ),
+              RaisedButton(
+                child: Text('Ok'),
+                onPressed: () => _finishEditing(),
+              )
+            ],
+          ),
+        ),
+      );
+
+  _finishEditing() {
+    widget.notesCubit.createNote(_titleController.text, _bodyController.text);
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _bodyController.dispose();
   }
 }
